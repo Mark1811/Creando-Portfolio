eaeaf11ad996593babd8a9f343a59b6a3a58de65
Marcos Aguero <78196802+Mark1811@users.noreply.github.com>
2021-12-14 23:50:25 -0300

    Primer commit del portfolio

---
+++ dinamico.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.efectoLe',{
+    strings:['<i Class="actitudes">Agil</i>', 
+            ' <i Class="actitudes">Creativo</i>',
+            '<i Class="actitudes">Trabajo en Equipo<i>',
+            '<i Class="actitudes">Proactivo<i>',
+            '<i Class="actitudes">Responsable<i>'
+           ],
+
+ typeSpeed: 75,
+ backSpeed: 0,
+ cursorChar: '/',
+ shuffle: false,
+ smartBackspace: false,
+ loop: true,
+ backDelay: 1500
+});
